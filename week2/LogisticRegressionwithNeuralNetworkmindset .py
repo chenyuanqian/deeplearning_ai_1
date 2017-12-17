91c6a275869b71febca305d471625c16526956c1
chenyuanqian <yuanqianchen@163.com>
2017-12-17 23:11:19 +0800

    2017/12/17
    2017/12/17

--- week2/LogisticRegressionwithNeuralNetworkmindset .py
+++ week2/LogisticRegressionwithNeuralNetworkmindset .py
@@ -13,6 +13,71 @@ from scipy import ndimage
 from lr_utils import load_dataset
 
 
+def sigmoid(z):
+    s=1/(1+np.exp(-z))
+    return s
+
+def initialize_with_zeros(dim):
+    w=np.zeros((dim,1),float)
+    b=0
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))    
+    return w, b
+
+def propagate(w,b,X,Y):
+    m=X.shape[1]
+    A=w.T.dot(X)+b
+    a=sigmoid(A)
+    cost=-1*((Y.dot(np.log(a).T))+(1-Y).dot(np.log(1-a).T))/m
+    dw=X.dot((a-Y).T)/m
+    db=(a-Y).sum()/m
+    assert(dw.shape==w.shape)
+    assert(db.dtype==float)
+    cost=np.squeeze(cost)
+    assert(cost.shape==())
+    grads = {"dw": dw,
+             "db": db}
+    return grads, cost
+
+def optimize(w,b,X,Y,num_iterations,learning_rate,print_cost=False):
+    costs=[]
+    for i in range(num_iterations):
+        grads,cost=propagate(w,b,X,Y)
+        dw=grads["dw"]
+        db=grads["db"]
+        w=w-dw*learning_rate
+        b=b-db*learning_rate
+        
+        if i%100==0:
+            costs.append(cost)
+        if print_cost and i%100==0:
+            print ("Cost after iteration %i:%f" %(i,cost))
+    
+    params={"w":w,
+            "b":b}
+    grads={"dw":dw,
+           "db":db}
+    
+    return params,grads,costs
+
+
+def predict(w,b,X):
+    m=X.shape[1]
+    Y_prediction=np.zeros((1,m))
+    w=w.reshape(X.shape[0],1)
+    A=sigmoid(w.T.dot(X)+b)
+    
+    for i in range(A.shape[1]):
+        if A[0][i]<0.5:
+            Y_prediction[0][i]=0
+        else:
+            Y_prediction[0][i]=1
+    assert(Y_prediction.shape == (1, m))
+    return Y_prediction
+            
+    
+    
+
 if __name__=="__main__":
     # 2-Overview of the Problem set
     
@@ -57,5 +122,34 @@ if __name__=="__main__":
     
     #3 - General Architecture of the learning algorithm
     #4 - Building the parts of our algorithm
+    print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
+    
+    #4.2
+    dim = 2
+    w, b = initialize_with_zeros(dim)
+    print ("w = " + str(w))
+    print ("b = " + str(b))
+    #4.3
+    w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+    grads, cost = propagate(w, b, X, Y)
+    print ("dw = " + str(grads["dw"]))
+    print ("db = " + str(grads["db"]))
+    print ("cost = " + str(cost))
+    #4.4
+    params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+    print ("w = " + str(params["w"]))
+    print ("b = " + str(params["b"]))
+    print ("dw = " + str(grads["dw"]))
+    print ("db = " + str(grads["db"]))
+    
+    
+    w = np.array([[0.1124579],[0.23106775]])
+    b = -0.3
+    X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+    print ("predictions = " + str(predict(w, b, X)))
+    
+
+
 
 
